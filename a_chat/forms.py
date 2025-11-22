7265541feb660b777b74dec639f3489744de5a81
SkyRaks <nano.voloshin2016@gmail.com>
2025-11-22 12:24:57 -0500

    added working form to the page

---
+++ a_chat/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from django import forms
+from .models import *
+
+class ChatMessageForm(ModelForm):
+    class Meta:
+        model = ChatMessage
+        fields = ['body']
+        widgets = {
+            'body': forms.TextInput(attrs={'placeholder': 'add message', 'class': 'text-black', 'max_length': '300', 'autofocus': True})
+        }
--- a_chat/views.py
+++ a_chat/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from .forms import *
 from .models import *
 
 # Create your views here.
@@ -12,7 +13,6 @@ def chat_view(request, username):
 
     other_user = User.objects.get(username=username)
     my_chatrooms = request.user.chat_groups.all()
-
     chatroom = None
 
     for room in my_chatrooms:
@@ -23,7 +23,17 @@ def chat_view(request, username):
     if chatroom == None:
         chatroom = ChatGroup.objects.create()
         chatroom.members.add(other_user, request.user)
-
+    # at this point we found or created chatroom
     chat_messages = chatroom.chat_messages.all()[:30]
 
-    return render(request, 'a_chat/chat.html', {'chat_messages': chat_messages})
+    form = ChatMessageForm()
+    if request.method == 'POST':
+        form = ChatMessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chatroom
+            message.save()
+            return redirect('chat', username=username)
+
+    return render(request, 'a_chat/chat.html', {'chat_messages': chat_messages, 'form': form})
