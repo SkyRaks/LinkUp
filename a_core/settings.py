ff30beb33055319f2206e72a26e1959f8a1a7977
SkyRaks <nano.voloshin2016@gmail.com>
2026-03-06 19:37:48 -0500

    minor fix2

--- a_core/settings.py
+++ a_core/settings.py
@@ -13,9 +13,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECTET_KEY = os.getenv('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
